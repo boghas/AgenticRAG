ab008aee494e40f882bbd584327705e2b0609421
Bogdan Hasan <bogdan.hasan@yahoo.com>
2026-06-02 00:46:30 +0300

    Added the graph

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from graph.graph import app
+
+
 def main():
     print("Hello from agenticrag-langgraph!")
+    print(app.invoke(input={"question": "what is agent memory"}))
 
 
 if __name__ == "__main__":
