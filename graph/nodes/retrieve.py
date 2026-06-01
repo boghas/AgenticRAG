3970ed198c194083c8e6ebb48d99953276d371bd
Bogdan Hasan <bogdan.hasan@yahoo.com>
2026-06-01 22:55:37 +0300

    Added the retrieve node

---
+++ graph/nodes/retrieve.py
@@ -0,0 +1,13 @@
+from typing import Any
+
+from graph.state import GraphState
+from ingestion import retriever
+
+
+def retrieve(state: GraphState) -> dict[str, Any]:
+    print("---Retrieve---")
+    question = state["question"]
+
+    documents = retriever.invoke(question)
+
+    return {"documents": documents, "question": question}
